a3fb13153551f44a835fb8f1f9722ef1a7a8e30a
Ricki Steiner <Rickisteiner@gmail.com>
2015-05-14 15:46:39 -0400

    fixing server with correct routes

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
 var express = require('express');
 var app = express();
+var public_dir = './public/';
+
+app.get('/', function(req, res) {
+	res.sendfile(public_dir + 'index.html');
+});
+
+app.get('/writing', function(req, res) {
+	res.sendfile(public_dir + 'writting.html');
+});
+
+app.get('/projects', function(req, res) {
+	res.sendfile(public_dir + 'work.html');
+});
 
 app.use(express.static(__dirname + '/public'));
+
 
 app.listen(3000);
